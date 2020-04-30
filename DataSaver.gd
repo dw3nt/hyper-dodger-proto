e46af0724f5344172b4d3df5a8cad2ea14d81d3f
dw3nt <rick@dualwieldent.com>
2020-04-29 21:33:17 -0700

    DataSaver with settings

--- DataSaver.gd
+++ DataSaver.gd
@@ -1,13 +1,25 @@
 extends Node
 
 const SCORE_PATH = "user://score.data"
+const SETTINGS_PATH = "user://settings.data"
 
 var scoreFile
 var score
 var highscore
 
+var settingsFile
+var musicEnabled
+var musicVol
+var sfxEnabled
+var sfxVol
+
 
 func _ready():
+	initScore()
+	initSettings()
+	
+	
+func initScore():
 	scoreFile = File.new()
 	if !scoreFile.file_exists(SCORE_PATH):
 		score = 0
@@ -17,6 +29,22 @@ func _ready():
 		var scoreData = readScores()
 		score = scoreData["score"]
 		highscore = scoreData["highscore"]
+		
+		
+func initSettings():
+	settingsFile = File.new()
+	if !settingsFile.file_exists(SETTINGS_PATH):
+		musicEnabled = true
+		musicVol = 100
+		sfxEnabled = true
+		sfxVol = 100
+		writeSettings(musicEnabled, musicVol, sfxEnabled, sfxVol)
+	else:
+		var settingsData = readSettings()
+		musicEnabled = settingsData["musicEnabled"]
+		musicVol = settingsData["musicVol"]
+		sfxEnabled = settingsData["sfxEnabled"]
+		sfxVol = settingsData["sfxVol"]
 	
 
 func writeScores(_score, _highscore):
@@ -31,3 +59,20 @@ func readScores():
 	scoreFile.close()
 	return data
 	
+	
+func writeSettings(_musicEnabled, _musicVol, _sfxEnabled, _sfxVol):
+	settingsFile.open(SETTINGS_PATH, File.WRITE)
+	settingsFile.store_var({
+		"musicEnabled": _musicEnabled, 
+		"musicVol": _musicVol, 
+		"sfxEnabled": _sfxEnabled, 
+		"sfxVol": _sfxVol 
+	})
+	settingsFile.close()
+	
+	
+func readSettings():
+	settingsFile.open(SETTINGS_PATH, File.READ)
+	var data = settingsFile.get_var()
+	settingsFile.close()
+	return data
--- Settings.gd
+++ Settings.gd
@@ -2,14 +2,25 @@ extends Control
 
 signal settings_main_menu
 
+onready var musicToggle = $CenterWrap/SettingsWrap/MusicWrap/MusicToggle
+onready var musicVol = $CenterWrap/SettingsWrap/MusicWrap/MusicVolumeWrap/MusicVolume
+onready var sfxToggle = $CenterWrap/SettingsWrap/SfxWrap/SfxToggle
+onready var sfxVol = $CenterWrap/SettingsWrap/SfxWrap/SfxVolumeWrap/SfxVolume
 onready var anim = $Anim
 
 
 func startUp():
+	var settingsData = DataSaver.readSettings()
+	musicToggle.pressed = settingsData["musicEnabled"]
+	musicVol.value = settingsData["musicVol"]
+	sfxToggle.pressed = settingsData["sfxEnabled"]
+	sfxVol.value = settingsData["sfxVol"]
+	
 	anim.play("fade_in")
 	
 	
 func tearDown():
+	DataSaver.writeSettings(musicToggle.pressed, musicVol.value, sfxToggle.pressed, sfxVol.value)
 	anim.play("fade_out")
 
 
